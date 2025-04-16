028c1684957e8d786cbbc7261676aae5ad71a29c
MoeHashem <108379321+MoeHashem@users.noreply.github.com>
2025-04-16 16:46:56 -0400

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -74,7 +74,7 @@ with open(OUTPUT_FILE, "w") as f:
 
     for file in puzzle_files:
         name = os.path.splitext(file)[0]              # e.g., "CW1 - Humblex"
-        cw_number = name.split(" - ")[0].strip()      # Add .strip() to remove extra spaces
+        cw_number = name.split("-")[0].strip()      # Add .strip() to remove extra spaces
         icon_path = f"{ICON_FOLDER}/{cw_number}_icon.png"
         puzzle_path = f"{PUZZLE_FOLDER}/{file}"
 
