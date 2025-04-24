20e41802b0d06721f7291f965975a7a9e8b682a0
MoeHashem <108379321+MoeHashem@users.noreply.github.com>
2025-04-24 16:32:35 -0400

    add favicon

--- generate_index.py
+++ generate_index.py
@@ -14,6 +14,7 @@ with open(OUTPUT_FILE, "w") as f:
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Crossword Collection</title>
+  <link rel="icon" href="../favicon.png" type="image/png">
   <style>
     body {
       font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
